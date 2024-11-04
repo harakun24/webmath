c4ecb864ac87bc6cf07129886d73ffab5f331d3f
dimas oppo <dimasoppo448@gmail.com>
2024-11-04 12:42:55 +0700

    bola.1.1

--- solid/bola/js/index.js
+++ solid/bola/js/index.js
@@ -264,6 +264,9 @@ function simpanFn() {
 window.onload = function () {
 
     katex.render(`x`, document.querySelector('.ex'))
+    katex.render(`x`, document.querySelectorAll('.ex')[1])
     katex.render(`y`, document.querySelector('.ey'))
+    katex.render(`x`, document.querySelectorAll('.ey')[1])
     katex.render(`z`, document.querySelector('.ez'))
+    katex.render(`x`, document.querySelectorAll('.ez')[1])
 }
